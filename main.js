815824e9d24eecf99fb1b9da90ae4c30264cdb56
Jack Sebben <jacksebben11@hotmail.com>
2023-06-03 11:10:13 -0400

    Added more functionality to the event listener function for the 'endBtn'. Now when a user clicks on the end time not only will their end time display, but in the following row the elapsed time between start and end times will display. There are also error messages that will display if the user tries to click the start or end buttons out of sequence, this is tracked using the sessionActive variable that tracks the state (ex: user pressed startBtn when a sesssion is active). Tested and working in Mozilla and Brave Browsers.

--- main.js
+++ main.js
@@ -1,37 +1,64 @@
-const startBtn = document.getElementById("start-btn")
-const endBtn = document.getElementById("end-btn")
-const mainTable = document.getElementById("main-table")
+// --> SELECTED ELEMENTS <-- //
+const startBtn = document.getElementById("start-btn");
+const endBtn = document.getElementById("end-btn");
+const mainTable = document.getElementById("main-table");
+let sessionActive = false;
+let startTime;
+
+// --> FUNCTIONALITY <-- // 
 
 function insertRow() {
-    const newRow = mainTable.insertRow(-1)
+    const newRow = mainTable.insertRow(-1);
     for (let i = 0; i < 5; i++) {
-        newRow.insertCell(i)
+        newRow.insertCell(i);
     }
 }
 
 function getCurrentDate() {
-    let year = new Date().getFullYear()
-    let month = new Date().getMonth()
-    let day = new Date().getDate()
-    return `${year}/${month}/${day}`
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1; // JavaScript months are 0-based, so we add 1
+    let day = date.getDate();
+    return `${year}/${month}/${day}`;
 }
 
 function getCurrentTime() {
-    let hours = new Date().getHours()
-    let minutes = new Date().getMinutes()
-    let seconds = new Date().getSeconds()
-    return `${hours}:${minutes}:${seconds}`
+    let date = new Date();
+    let hours = pad(date.getHours(), 2);
+    let minutes = pad(date.getMinutes(), 2);
+    let seconds = pad(date.getSeconds(), 2);
+    return `${hours}:${minutes}:${seconds}`;
 }
 
-getCurrentDate()
-getCurrentTime()
+function pad(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+
+// --> EVENT LISTENERS <-- //
 
 startBtn.addEventListener('click', function() {
-    insertRow()
-    mainTable.rows[mainTable.rows.length - 1].cells[0].innerHTML = getCurrentDate()
-    mainTable.rows[mainTable.rows.length - 1].cells[1].innerHTML = getCurrentTime()
-})
+    if (!sessionActive) {
+        insertRow();
+        mainTable.rows[mainTable.rows.length - 1].cells[0].innerHTML = getCurrentDate();
+        startTime = new Date(); // Record the start time
+        mainTable.rows[mainTable.rows.length - 1].cells[1].innerHTML = getCurrentTime();
+        sessionActive = true;
+    } else alert("Error: Start-time already entered");
+});
 
 endBtn.addEventListener('click', function() {
-    mainTable.rows[mainTable.rows.length - 1].cells[3].innerHTML = getCurrentTime()
-})
+    if (sessionActive) {
+        mainTable.rows[mainTable.rows.length - 1].cells[3].innerHTML = getCurrentTime();
+        const endTime = new Date(); // Get the end time
+        const elapsedMs = endTime - startTime; // Calculate the elapsed time in milliseconds
+        const elapsedSecs = Math.round(elapsedMs / 1000); // Convert to seconds
+        const hours = pad(Math.floor(elapsedSecs / 3600), 2); // Get the hours part
+        const mins = pad(Math.floor((elapsedSecs % 3600) / 60), 2); // Get the minutes part
+        const secs = pad(elapsedSecs % 60, 2); // Get the seconds part
+        const elapsedTime = `${hours}:${mins}:${secs}`;
+        mainTable.rows[mainTable.rows.length - 1].cells[4].innerHTML = elapsedTime;
+        sessionActive = false;
+    } else alert("Error: Start-time required");
+});
